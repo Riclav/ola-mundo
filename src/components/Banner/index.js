c62e0a1cc167e9b267992c685c01888ad0736cf8
Riclav Stolf <riclavstolf@hotmail.com>
2024-02-13 23:18:06 -0300

    Projeto aula nova react routers

---
+++ src/components/Banner/index.js
@@ -0,0 +1,40 @@
+import styles from './Banner.module.css';
+import circuloColorido from 'assets/circulo_colorido.png';
+
+import minhaFoto from 'assets/minha_foto.png';
+
+
+export default function Banner() {
+    return (
+        <div className={styles.banner}>
+            <div className={styles.apresentacao}>
+                <h1 className={styles.titulo}>
+                    Olá, Mundo!
+                </h1>
+                <p className={styles.paragrafo}>
+                    Boas vindas ao meu espaço pessol! Eu sou Riclav Stolf, aluno da escola de Front-end Alura. Aqui compartilho vários conhecimentos, espero que aprenda algo novo:)
+                </p>
+            </div>
+
+            <div className={styles.imagens}>
+                <img
+                    className={styles.circuloColorido}
+                    src={circuloColorido}
+                    aria-hidden={true}
+                />
+
+                <img
+                    className={styles.minhaFoto}
+                    src={minhaFoto} 
+                    alt="Foto do Ric"
+                />
+            </div>
+        </div>
+
+
+
+    )
+
+
+
+}
