69fdfffb759aed46c0f9bfce1d605941f1607de2
Riclav Stolf <riclavstolf@hotmail.com>
2024-02-19 21:39:26 -0300

    Finalizado

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -11,7 +11,7 @@ export default function Banner() {
                 </h1>
 
                 <p className={styles.paragrafo}>
-                    Boas vindas ao meu espaço pessoal! Eu sou Antônio Evaldo, instrutor de Front-end da Alura. Aqui compartilho vários conhecimentos, espero que aprenda algo novo :)
+                    Boas vindas ao meu espaço pessoal! Eu sou Riclav Stolf, aluno de Front-end da Alura. Aqui compartilho vários conhecimentos, espero que aprenda algo novo :)
                 </p>
             </div>
 
@@ -26,7 +26,7 @@ export default function Banner() {
                 <img
                     className={styles.minhaFoto}
                     src={minhaFoto}
-                    alt="Foto do Antônio Evaldo sorrindo"
+                    alt="Foto do Ric"
                 />
             </div>
         </div>
--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -11,34 +11,31 @@ export default function SobreMim() {
             titulo="Sobre mim"
         >
             <h3 className={styles.subtitulo}>
-                Olá, eu sou o Antônio!
+                Olá, eu sou o Ric!
             </h3>
 
             <img
                 src={fotoSobreMim}
-                alt="Foto do Antônio Evaldo sorrindo"
+                alt="Foto do Ric"
                 className={styles.fotoSobreMim}
             />
 
             <p className={styles.paragrafo}>
-                Oi, tudo bem? Eu sou instrutor de Front-end na Alura e estou feliz de te ver por aqui.
+                Oi, tudo bem? Eu sou aluno de Front-end na Alura e estou feliz de te ver por aqui.
             </p>
             <p className={styles.paragrafo}>
-                Minha história com programação começou no Instituto Federal do Piauí (IFPI), quando fiz o ensino médio integrado ao curso de Informática. Eu aprendi lógica de programação e o básico de várias linguagens, como PHP, Python, Java, mas sem aprofundar muito em cada uma delas. Eu gostava muito de estudar programação, mas na época não fazia ideia de que trabalharia com isso hoje.
+                Minha história com programação começou na Universidade Internacional (UNINTER), onde me formtei no curso de análise e desenvolvimento de sistemas. Eu aprendi lógica de programação e o básico de várias linguagens, como PHP, Python, Java, mas sem aprofundar muito em cada uma delas. Eu gostava muito de estudar programação, mas na época não fazia ideia de que trabalharia com isso hoje.
             </p>
             <p className={styles.paragrafo}>
-                No ensino superior, escolhi cursar Engenharia Elétrica na Universidade Federal do Piauí (UFPI). Lá eu consegui entrar no PET (Programa de Educação Tutorial), um grupo acadêmico que contribui de diversas formas para o curso. Lá eu tive a oportunidade de estudar desenvolvimento web para ser um dos mantenedores do site deles.
+                Após o ensino superior, escolhi cursar Pós-Graduação na mesma instituição. Onde atualmente estou cursando dois cursos para enriquecer meus conhecimentos (Engenharia de software e Ciência de dados e Inteligência artificial).
             </p>
             <p className={styles.paragrafo}>
-                Com isso tive minha experiência de dev mais próxima da realidade, com prazos de implementação para o site, e aprendi muito enquanto codificava.
+                Desde o final de do ano de 2023, assumi novos desafios na empresa que trabalho, atuando com desenvolverdor de software, para as inumeras demandas dentro da engenharia.
             </p>
             <p className={styles.paragrafo}>
-                E foi enquanto eu estava no PET que decidi procurar um estágio em desenvolvimento web. Eu encontrei uma vaga na Alura para o Scuba Team e percebi que ela aliava duas paixões minhas: programação e educação. Para minha felicidade, fui escolhido para fazer parte do time.
-
+               Cada dia é uma nova experiência, e os cursos da Alura estão me guiando nesse longo caminho de aprendizado.
             </p>
-            <p className={styles.paragrafo}>
-                Desde então, tem sido aprenas aprendizados atrás de aprendizados. A Alura é uma escola não só para seus alunos e alunas, mas também para os colaboradores e colaboradoras. Hoje sou muito feliz de ter a oportunidade de trazer esses conteúdos ricos e encantadores para você. Espero que aprenda bastante!
-            </p>
+             
         </PostModelo>
     )
 }
